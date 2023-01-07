262c518d45e6df84965e127166c8323f8ba8ab50
jinweios <jinwei.peng@outlook.com>
2023-01-07 11:33:04 +0800

    feat: 插槽作用域

--- src/components/navbar/TabBody.vue
+++ src/components/navbar/TabBody.vue
@@ -1,16 +1,27 @@
 <template>
     <div>
         <!--匿名插槽，默认插槽-->
-        <slot></slot>
+        <!--默认值 backup-->
+        <slot>默认</slot>
         <!--具名插槽-->
         <div class="header">
-            <slot name="header"></slot>
+            <slot name="header" :user="user">
+                callback({user: user})
+                {{ user.name }}
+            </slot>
         </div>
     </div>
 </template>
 <script>
 export default {
     name: 'TabBody',
+    data: function() {
+        return {
+            user: {
+                name: '告警消息'
+            }
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
